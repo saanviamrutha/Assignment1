739184a06d5c4f51c3ec78c0d8c4206297ab1a55
saanviamrutha <102648734+saanviamrutha@users.noreply.github.com>
2022-04-10 17:34:34 +0530

    Add files via upload

---
+++ codes/pcode.py
@@ -0,0 +1,12 @@
+Angle_BAD=90
+Angle_ADB=55
+#Angle_BAD+Angle_ADB+Angle_ADB=180
+Angle_ABD=180-(90+Angle_ADB)
+#In a triangle, exterior angle is equal to sum of the two opposite interior angles.
+Angle_AOE=2*Angle_ABD
+y=Angle_AOE
+print("y=",y,chr(176),sep='')
+Angle_OAC=90
+x=180-(y+Angle_OAC)
+print("x=",x,chr(176),sep='')
+
